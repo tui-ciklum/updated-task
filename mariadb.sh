24ac3c2137767ee4247f33bedc8854ee266af9fc
tui-ciklum <107998191+tui-ciklum@users.noreply.github.com>
2022-07-07 16:59:16 +0530

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y mariadb-server
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
+#sudo systemctl start mariadb
+#sudo systemctl enable mariadb
+ service mariadb start
